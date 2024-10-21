d47c1ac84f728f90125471004f13cf852d8d3f90
lennynT02 <lenin.masapanta@epn.edu.ec>
2024-10-21 01:23:49 -0500

    Aplicacion 70% terminada

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -6,7 +6,33 @@ import { Component } from '@angular/core';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
+  
+  habilidades: string[] = [
+    'Comunicación efectiva',
+    'Adaptabilidad y flexibilidad',
+    'Inteligencia emocional'
+  ];
 
-  constructor() {}
+ 
+  tecnologias: string[] = [
+    'Python',
+    'HTML/CSS/JS',
+    'Java',
+    'C++',
+    'Manejo de base de datos con SQL'
+  ];
 
+  
+  educaciones: { institucion: string; descripcion: string; ubicacion: string }[] = [
+    {
+      institucion: 'Colegio Santa Ana',
+      descripcion: 'Bachillerato de Ciencias Básicas',
+      ubicacion: 'Quito, Ecuador'
+    },
+    {
+      institucion: 'Escuela Politécnica Nacional',
+      descripcion: 'Tecnología Superior en Desarrollo de Software',
+      ubicacion: 'Año: 2021 - Actualidad, Quito, Ecuador'
+    }
+  ];
 }
